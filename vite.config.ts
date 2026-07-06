5e4553308f4aa28b39c48b34e7b10b99d24b56d9
furkanized <furkanndurgun@gmail.com>
2026-07-06 17:44:24 +0300

    Fix production CSS minify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssMinify: false,
+  },
   preview: {
     allowedHosts: [
       "humble-achievement-production-94d8.up.railway.app"
